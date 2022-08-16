d48c4099ceb5154bfdeaef681b15390b11ac4ada
pengkol <108908182+pengkol@users.noreply.github.com>
2022-08-16 07:23:39 +0700

    Update and rename app/src/main/kotlin/com/gatcha/ang/ui/SubEditActivity.kt to app/src/main/kotlin/com/gandring/ang/ui/SubEditActivity.kt

--- app/src/main/kotlin/com/gandring/ang/ui/SubEditActivity.kt
+++ app/src/main/kotlin/com/gandring/ang/ui/SubEditActivity.kt
@@ -1,4 +1,4 @@
-package com.gatcha.ang.ui
+package com.gandring.ang.ui
 
 import android.os.Bundle
 import android.text.TextUtils
@@ -7,12 +7,12 @@ import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
 import com.google.gson.Gson
 import com.tencent.mmkv.MMKV
-import com.gatcha.ang.R
-import com.gatcha.ang.databinding.ActivitySubEditBinding
-import com.gatcha.ang.dto.SubscriptionItem
-import com.gatcha.ang.extension.toast
-import com.gatcha.ang.util.MmkvManager
-import com.gatcha.ang.util.Utils
+import com.gandring.ang.R
+import com.gandring.ang.databinding.ActivitySubEditBinding
+import com.gandring.ang.dto.SubscriptionItem
+import com.gandring.ang.extension.toast
+import com.gandring.ang.util.MmkvManager
+import com.gandring.ang.util.Utils
 
 class SubEditActivity : BaseActivity() {
     private lateinit var binding: ActivitySubEditBinding
